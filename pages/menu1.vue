155906df33dc12e6a528463ee3e905a542cb1d6d
coachmax <kitphokhin20721@gmail.com>
2018-05-03 14:05:04 +0700

    หน้าแสดง

---
+++ pages/menu1.vue
@@ -0,0 +1,6 @@
+<template>
+<ul>
+<li v-for="n in 8" :key="n"><nuxt-link :to="'/sample0' + n"> Sample0{{n}}</nuxt-link></li>
+</ul>
+</template>
+
---
+++ pages/sample03.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1> Hello {{name}} </h1>
+      <div>{{ upperName }}</div>
+      <input type="text" :value="name"><br>
+      <input type="text" v-model="name">
+
+  </div>
+</template>
+<script>
+export default {
+ data() {
+ return { name: "World" }
+ },
+ computed: {
+ upperName() {
+ return this.name.toUpperCase()
+ }
+ }
+}
+</script>
+
