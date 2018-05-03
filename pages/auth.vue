9a285b9d9e6ad6bfee0a955f832cf4888ed01c2c
Kampanat <Kampanat_it@live.com>
2018-05-03 16:54:05 +0700

    editLogin

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,6 +1,32 @@
 <template>
- <v-form v-model="valid">
-            <v-text-field
+         <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <!-- <v-btn
+                    icon
+                    large
+                    :href="source"
+                    target="_blank"
+                    slot="activator"
+                  > -->
+                    <v-icon large>code</v-icon>
+                  <!-- </v-btn> -->
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <v-btn icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank" slot="activator">
+                    <v-icon large>mdi-codepen</v-icon>
+                  </v-btn>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                   <v-text-field
               label="E-mail"
               v-model="email"
               :rules="emailRules"
@@ -17,8 +43,15 @@
                 :type="e1 ? 'password' : 'text'"
                 counter
             ></v-text-field>
-            <v-btn color="primary" flat nuxt to="/admin">Continue</v-btn>
-         </v-form>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" to="/admin">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
 </template>
 <script>
   export default {
