ad0f99a635211cb19204917e874f0071488eb7f6
Krulek <krusudruadee@gmail.com>
2018-05-06 10:43:38 +0700

    edit member page

--- pages/member.vue
+++ pages/member.vue
@@ -1,22 +1,25 @@
 <template>
   <section class="boxs">
     <article class="boxed">
-      <div class="thumbnail" style="background-image: url(https://wallpaper.thaiware.com/upload/wallpaper/2009_02/4543_4320_090224094719_9U.jpg)"></div>
+      <div class="thumbnail">
+        <img src="/KruJeab.jpg" alt="ระบบจัดการข้อมูลนักศึกษา" class="mb-5" />
+      </div>
       <h1>นายฐารรรฎร  เกรัมย์</h1>
       <p>วท.บุรีรัมย์</p>
     </article>
     <article class="boxed">
-      <div  class="thumbnail" style="background-image: url(http://www.desktopwallpaperhd.net/wallpapers/7/5/juegos-wallpapers-normal-nuevos-general-section-spoiler-76805.jpg)"></div>
+      <div  class="thumbnail"><img src="/KruMax.jpg" alt="ระบบจัดการข้อมูลนักศึกษา" class="mb-5" />
+      </div>
       <h1>นายกิตติ์โภคิน  ธนยศจินดารัชต์</h1>
       <p>วท.บุรีรัมย์</p>
     </article>
     <article class="boxed">
-      <div  class="thumbnail" snptyle="background-image: url(https://scontent-fbkk5-7.us-fbcdn.net/v1/t.1-48/1426l78O9684I4108ZPH0J4S8_842023153_K1DlXQOI5DHP/dskvvc.qpjhg.xmwo/w/data/710/710953-topic-ix-9.jpg)"></div>
+      <div  class="thumbnail"><img src="/Krulek.jpg" alt="ระบบจัดการข้อมูลนักศึกษา" class="mb-5" /></div>
       <h1>นางสุดฤดี  ประทุมชาติ</h1>
       <p>วอศ.อุบลราชธานี</p>
     </article>
     <article class="boxed">
-      <div  class="thumbnail" style="background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT-UnZr2Mk4MBobv4I0f1c-wYiAPf0eAvaAgSZzjKhV9g0lxEscQ)"></div>
+      <div  class="thumbnail"><img src="/KruBig1.jpg" alt="ระบบจัดการข้อมูลนักศึกษา" class="mb-5" /></div>
       <h1>นายกัมปนาท หวะสุวรรณ</h1>
       <p>วท.ชัยภูมิ</p>
     </article>
@@ -35,10 +38,10 @@
   box-shadow: 2px 2px #aaa;
   margin: 10px;
 }
-.thumbnail{
+.thumbnail,img{
   background-position: center;
   background-size: cover;
-  width: 100%;
+  width: 260px;
   height: 200px;
 }
 </style>
